70fa3d80b214ff252705991702ef96a01c953c72
Justine Joshua Tan <justinejoshuatan@yahoo.com>
2026-01-31 20:55:33 +0800

    Update for implementing stopped and active vehicle
    STILL INCOMPLETE
    Missing:
    1. Differentiate between stopped and active (moving) vehicles
    
    Future implementations:
    1. Stop light to expand active vehicles
    2. Detect on-street parking

--- detect_osp.py
+++ detect_osp.py
@@ -8,8 +8,12 @@ from ultralytics import YOLO
 def main():
     # Input video title (within path)
     print("Input driving video name (without .mp4): ")
-    video_name = input()
-    video_path = "videos/" + video_name + ".mp4"
+    # video_name = input()
+    # video_path = "videos/" + video_name + ".mp4"
+
+    # TEMP: Just to make things quick
+    video_path = "videos/makati_city_hall.mp4"
+
 
     print("Playing: " + video_path)
 
@@ -36,8 +40,8 @@ def main():
     
     # Optical Flow setup, using Lucas-Kanade Optical Flow, meaning this is sparse, not dense
     # ShiTomasi corner detection parameters
-    feature_params = dict(maxCorners = 100,
-                            qualityLevel = 0.3,
+    feature_params = dict(maxCorners = 200,
+                            qualityLevel = 0.1,
                             minDistance = 7,
                             blockSize = 7)
     
@@ -59,60 +63,17 @@ def main():
     # mask image to draw lines
     mask = np.zeros_like(prev_frame)
     frame_count = 0
-    
+
+    car_consistency = {}
+    car_vectors_map = {} # temp bucket for optical flow arrows by Car ID for current frame
+    if 'car_status_memory' not in locals(): car_status_memory = {} # save car status, whether stopped, moving, or neither
+
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             # If there are no more frames (video has ended)
             break
-
-        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        # cv2.calcOpticalFlowPyrLK - for optical flow between two frames
-        # p1 - new positions of p0 points, st - status of flow between points, err - errors
-        p1, st, err = cv2.calcOpticalFlowPyrLK(prev_gray, frame_gray, p0, None, **lk_params)
-
-        if p1 is not None and st is not None:
-            good_next = p1[st==1] # good_next - good points from next frame, st == 1 means flow between points exists
-            good_prev = p0[st==1] # good_prev - good points from previous frame
-
-            motion_vectors = good_next - good_prev # motion_vectors - distance between two similar points in two different frames
-
-            # if there is distance between motion vectors from different frames
-            if motion_vectors.shape[0] > 0:
-                global_motion_vector = np.median(motion_vectors, axis=0) # main median vector denoting the motion of the camera (based on the vehicle)
-                
-                for i, (new, old) in enumerate(zip(good_next, good_prev)):
-                    # (a, b) are points from new frame, (c, d) are points from old frame
-                    a, b = new.ravel().astype(int)
-                    c, d = old.ravel().astype(int)
-                    mask = cv2.line(mask, (a, b), (c, d), (0, 255, 0), 1)
-                    frame = cv2.circle(frame, (a, b), 5, (0, 255, 0), -1)
-
-                # Arrow denoting the movement of the vehicle based on the global motion vector
-                center = (int(frame.shape[1] * 0.5), int(frame.shape[0] * 0.1))
-                end_point = (int(center[0] + global_motion_vector[0] * 10), int(center[1] + global_motion_vector[1] * 10))
-                frame = cv2.arrowedLine(frame, center, end_point, (0, 0, 255), 3, tipLength=0.5)
-
-            # otherwise, if there are no points, reset global motion vector to 0, 0
-            else:
-                global_motion_vector = np.array([0, 0])
-
-        else:
-            # If there are no points, create new points to start with
-            p0 = cv2.goodFeaturesToTrack(prev_gray, mask = None, **feature_params)
-            mask = np.zeros_like(prev_frame)
-            global_motion_vector = np.array([0, 0])
-
-        prev_gray = frame_gray.copy()
-        p0 = good_next.reshape(-1, 1, 2) if 'good_next' in locals() and good_next.shape[0] > 0 else cv2.goodFeaturesToTrack(prev_gray, mask = None, **feature_params)
-
-        frame_count += 1
-        # Reset mask and older point to the second most recent frame
-        if frame_count % 30 == 0:
-            mask = np.zeros_like(prev_frame)
-            p0 = cv2.goodFeaturesToTrack(prev_gray, mask = None, **feature_params)
-
+        
         result = model(frame, verbose=False)[0]
 
         # Load in predictions to Supervision, specifically Ultralytics because inference model comes from there
@@ -122,28 +83,223 @@ def main():
         detections = tracker.update_with_detections(detections)
         detections = smoother.update_with_detections(detections)
 
-        # Removes frames without tracker ID
+        # Filter that removes frames without tracker ID
         if detections.tracker_id.any():
             mask_filter = [tracker_id is not None for tracker_id in detections.tracker_id]
             detections = detections[(detections.confidence > 0.3) & mask_filter]
-        
+
+        global_motion_vector = np.array([0, 0])
+
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        background_vectors = []
+
+        if detections.xyxy.shape[0] > 0:
+            for tracker_id in detections.tracker_id:
+                car_vectors_map[tracker_id] = []
+
+        # check to ignore if there are no p0 points
+        if p0 is not None:
+            # cv2.calcOpticalFlowPyrLK - for optical flow between two frames
+            # p1 - new positions of p0 points, st - status of flow between points, err - errors
+            p1, st, err = cv2.calcOpticalFlowPyrLK(prev_gray, frame_gray, p0, None, **lk_params)
+
+            if p1 is not None and st is not None:
+                good_next = p1[st==1] # good_next - good points from next frame, st == 1 means flow between points exists
+                good_prev = p0[st==1] # good_prev - good points from previous frame
+
+                """
+                clean_motion_vectors = []
+
+                # GET FRAME DIMENSIONS FOR EDGE FILTERING
+                h, w = frame.shape[:2]
+                edge_margin = 20 # Ignore points within 20px of edge
+                
+                # SPEED LIMIT (Magnitude Cap)
+                # If a point moves more than 20 pixels in 1 frame, it's likely a glitch/error
+                max_jump_limit = 20.0
+                """
+
+                for i, (new, old) in enumerate(zip(good_next, good_prev)):
+                    # (a, b) are points from new frame, (c, d) are points from old frame, used for line
+                    a, b = new.ravel().astype(int)
+                    c, d = old.ravel().astype(int)
+                    motion_vector = new - old
+                    
+                    # MAGNITUDE CHECK (Filter glitches)
+                    if np.linalg.norm(motion_vector) > 100: continue 
+
+                    # CHECK: Is this point inside a Car Box?
+                    point_on_car = False
+                    
+                    if detections.xyxy.shape[0] > 0:
+                        for j, box in enumerate(detections.xyxy):
+                            x1, y1, x2, y2 = box
+                            tracker_id = detections.tracker_id[j]
+                            
+                            # If point is inside this car's box
+                            if x1 < a < x2 and y1 < b < y2:
+                                # DEBUG: Print one example to check the scale
+                                # if i == 0 and len(detections) > 0:
+                                #     print(f"--- DEBUG COORDINATES ---")
+                                #     print(f"Point: ({a}, {b})")
+                                #     print(f"Box:   ({x1:.1f}, {y1:.1f}) to ({x2:.1f}, {y2:.1f})")
+                                #     print(f"Is Inside? {x1 < a < x2 and y1 < b < y2}")
+                                #     print(f"-------------------------")
+                                    
+                                car_vectors_map[tracker_id].append(motion_vector)
+                                point_on_car = True
+                                # mask = cv2.line(mask, (a, b), (c, d), (0, 255, 0), 1)
+                                # Visual: Draw RED dot for car points
+                                cv2.circle(frame, (a, b), 3, (0, 0, 255), -1)
+                                # if i == 0 and j == 0: # Only print for the first point and first box to save console space
+                                #     print(f"DEBUG CHECK:")
+                                #     print(f"  Point: {a}, {b} (Type: {type(a)})")
+                                #     print(f"  Box: {x1}, {y1}, {x2}, {y2} (Type: {type(x1)})")
+                                break
+                    
+                    # If NOT on a car, it's Background
+                    if not point_on_car:
+                        background_vectors.append(motion_vector)
+                        # mask = cv2.line(mask, (a, b), (c, d), (0, 255, 0), 1)
+                        # Visual: Draw GREEN dot for background points
+                        cv2.circle(frame, (a, b), 3, (0, 255, 0), -1)
+
+                # Draw the Arrow
+                # center = (int(frame.shape[1]*0.5), int(frame.shape[0]*0.1))
+                # end_point = (int(center[0] + global_motion_vector[0]*7), int(center[1] + global_motion_vector[1]*7))
+                # cv2.arrowedLine(frame, center, d, (0, 0, 255), 3, tipLength=0.5)
+                
+            else:
+                p0 = cv2.goodFeaturesToTrack(prev_gray, mask=None, **feature_params)
+                mask = np.zeros_like(prev_frame)
+                global_motion_vector = np.array([0, 0])
+
+        global_motion_vector = np.array([0.0, 0.0])
+        # IMPORTANT TO NOTE:
+        if len(background_vectors) > 0:
+            global_motion_vector = np.median(background_vectors, axis=0)
+
+        # Draw Global Arrow
+        h, w = frame.shape[:2]
+        cv2.arrowedLine(frame, (w//2, h//2), 
+                       (int(w/2 + global_motion_vector[0]*5), int(h/2 + global_motion_vector[1]*5)), 
+                       (0, 0, 255), 3, tipLength=0.3)
+
+        status_labels = []
+        labels = []
+
         # Proceed only if there are detections left after filtering
         if detections.xyxy.shape[0] > 0:
-            labels = [
-                f"{tracker_id} {class_name} {confidence:0.2f}"
-                for tracker_id, class_name, confidence
-                in zip(detections.tracker_id, detections['class_name'], detections.confidence)
-            ]
+            for i in range(detections.xyxy.shape[0]):
+                tracker_id = detections.tracker_id[i]
+                bbox = detections.xyxy[i]
+
+                my_vectors = car_vectors_map.get(tracker_id, [])
+                
+                """
+                # --- DEBUG VISUALIZATION START ---
             
+                # 1. Draw the Car's Motion Vector (Blue Arrow)
+                # This lets you compare it visually to the Big Red Global Arrow
+                start_pt = (int(cx), int(cy))
+                # Scale up by 3 so we can see it clearly
+                end_pt = (int(cx + avg_car_vector[0]*3), int(cy + avg_car_vector[1]*3))
+                cv2.arrowedLine(frame, start_pt, end_pt, (255, 255, 0), 2, tipLength=0.3)
+
+                # 2. Print the MATH on the screen
+                # Format: "Diff vs Threshold"
+                # If Diff (5.0) < Thresh (8.0), it stays Parked. You need to see this!
+                debug_text = f"D:{diff:.1f} / T:{dynamic_threshold:.1f}"
+                cv2.putText(frame, debug_text, (int(bbox[0]), int(bbox[1])-10), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
+            
+                # --- DEBUG VISUALIZATION END ---
+                """
+
+                if len(my_vectors) < 3:
+                    # Not enough points on this car to decide (Too far or smooth)
+                    # Fallback: Assume PARKED or keep previous state
+                    # status = "NONE"
+                    # avg_car_vector = np.array([0.0, 0.0])
+
+                    # IF car_consistency[tracker_id] it will return error KeyError: np.int64(2)
+                    current_score = car_consistency.get(tracker_id, 0)
+
+                    if current_score > 4:
+                        status = "ACTIVE"
+                    else:
+                        status = "STOPPED"
+                else:
+                    # Calculate Car Arrow
+                    avg_car_vector = np.median(my_vectors, axis=0)
+                    
+                    # --- THE COMPARISON (Apples to Apples) ---
+                    # Now we compare two Optical Flow vectors. 
+                    # If the car is STOPPED, these vectors should be nearly IDENTICAL.
+                    
+                    diff_vector = avg_car_vector - global_motion_vector
+                    magnitude_diff = np.linalg.norm(diff_vector)
+                    
+                    # Dynamic Threshold logic (Simplified)
+                    # If the camera is moving fast (high global speed), we allow more error
+                    global_speed = np.linalg.norm(global_motion_vector)
+                    threshold = 1.5 + (global_speed * 0.5)
+                    
+                    # Consistency Logic
+                    if tracker_id not in car_consistency: car_consistency[tracker_id] = 0
+                    
+                    if magnitude_diff > threshold:
+                        car_consistency[tracker_id] += 1
+                    else:
+                        car_consistency[tracker_id] -= 1
+                        
+                    car_consistency[tracker_id] = max(-5, min(10, car_consistency[tracker_id]))
+                    previous_status = car_status_memory.get(tracker_id, "NONE")
+
+                    # HYSTERESIS CHECK
+                    if car_consistency[tracker_id] >= 4:
+                        status = "ACTIVE"
+                    elif car_consistency[tracker_id] <= 0:
+                        status = "STOPPED"
+                    else:
+                        status = previous_status
+                    
+                    car_status_memory[tracker_id] = status
+
+                    # DEBUG FOR CHECKING car_consistency
+                    mov_track = car_consistency[tracker_id]
+
+                    # VISUAL DEBUG: Draw the Car Arrow (Blue)
+                    cx, cy = int((bbox[0]+bbox[2])/2), int((bbox[1]+bbox[3])/2)
+                    cv2.arrowedLine(frame, (cx, cy),
+                                   (int(cx + avg_car_vector[0]*5), int(cy + avg_car_vector[1]*5)), 
+                                   (255, 255, 0), 2, tipLength=0.3)
+                    
+                    # Print Diff Score
+                    cv2.putText(frame, f"{magnitude_diff:.1f}/{threshold:.1f}", (int(bbox[0]), int(bbox[1])-5), 
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
+
+            for i in range(detections.xyxy.shape[0]):
+                conf = detections.confidence[i]
+                class_id = detections.class_id[i]
+                class_name = model.names[class_id]
+                
+
+                final_label = f"{class_name} {conf:.2f} {status} {mov_track}"
+                status_labels.append(final_label)
+
             annotated_frame = box_annotator.annotate(
                 scene=frame.copy(),
                 detections=detections
+                # color=color
             )
 
             annotated_frame = label_annotator.annotate(
                 scene=annotated_frame,
                 detections=detections,
-                labels=labels
+                labels=status_labels
+                # color=color
             )
 
             # Show annotated frame using bounding boxes and labels
@@ -153,6 +309,31 @@ def main():
         else:
             # Otherwise, show the original frame in a different window if there are no detections
             cv2.imshow("On-Street Parking Detection", frame)
+
+        if p1 is not None:
+            p0 = good_next.reshape(-1, 1, 2)
+        else:
+            p0 = None
+        # if 'good_next' in locals() and good_next.shape[0] > 0 else cv2.goodFeaturesToTrack(prev_gray, mask = None, **feature_params)
+
+        frame_count += 1
+        # Reset mask and older point to the second most recent frame
+        if frame_count % 30 == 0:
+            mask = np.zeros_like(prev_frame)
+            p0 = cv2.goodFeaturesToTrack(frame_gray, mask=None, **feature_params)
+        
+        elif p0 is None or len(p0) < 100:
+            mask = np.zeros_like(prev_frame)
+            new_points = cv2.goodFeaturesToTrack(prev_gray, mask = None, **feature_params)
+
+            if new_points is not None:
+                if p0 is not None:
+                    p0 = np.concatenate((p0, new_points), axis=0)
+                else:
+                    p0 = new_points
+
+
+        prev_gray = frame_gray.copy()
 
         # Press 'q' to quit
         if cv2.waitKey(1) & 0xFF == ord("q"):
